c05f2a8a0398e69ab06d551389fbde5fe4b6dcb5
aweel <jomeltiozon@gmail.com>
2020-12-09 13:26:03 +0800

    Remove conn

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db   = 'timekeeping';
-$user = 'admin';
-$pass = 'PUTANGINAmo007';
+$user = '';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
